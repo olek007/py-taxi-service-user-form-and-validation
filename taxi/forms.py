844a7c3b7d9a6833944638a516bc07f9698ea39a
Aleksander Rokita <kelor2@o2.pl>
2025-05-05 11:36:41 +0200

    Fix

--- taxi/forms.py
+++ taxi/forms.py
@@ -51,7 +51,7 @@ class DriverLicenseUpdateForm(forms.ModelForm):
 
 
 class CarForm(forms.ModelForm):
-    drivers = forms.ModelChoiceField(
+    drivers = forms.ModelMultipleChoiceField(
         queryset=get_user_model().objects.all(),
         widget=forms.CheckboxSelectMultiple
     )
